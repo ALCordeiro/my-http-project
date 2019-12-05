521f08d3d52ed690eabc8ac4460cc5f1330f3570
ALCordeiro <acordeiro@daitan.com>
2019-12-05 19:06:50 -0300

    Buttons structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExampleHttpComponent } from './example-http/example-http.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleHttpComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/example-http/example-http.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example-http',
+  templateUrl: './example-http.component.html',
+  styleUrls: ['./example-http.component.css']
+})
+export class ExampleHttpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
